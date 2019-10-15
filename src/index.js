58e1c960d7051d2c199d8bb2d7d0471b7f8a4002
zhangyanling77 <18783226594@163.com>
2019-10-15 10:00:21 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from "react-redux"
 import './index.css'
 import App from './router'
-import configureStore from "./store"
-
-const store = configureStore()
 
 ReactDOM.render(
-  <Provider store={store}>
-      <App />
-  </Provider>, 
+  <App />, 
   document.getElementById('root')
 )
 
